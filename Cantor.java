ef9ba9b772bae3c38b64f9c892215d16624df1cc
Dennis Fundi <84317264+fundid@users.noreply.github.com>
2026-05-05 10:35:09 +0300

    Refactor Cantor class and improve code structure

--- Cantor.java
+++ Cantor.java
@@ -1,139 +1,52 @@
-// Java program to print up to the N-th term in
+import java.util.Scanner;
 
-// George Cantor set of rational number
+class Cantor {
 
-import java.io.*;
+    static void georgeCantor(int n) {
 
- import java.util.*;
+        int i = 1; // numerator
+        int j = 1; // denominator
+        int k = 1; // term counter
 
-class Cantor
+        while (k < n) {
 
-{
-
-    static void georgeCantor(int n)
-
-    { 
-
-        // let i = numerator
-
-        int i = 1; 
-
-         
-
-        // let j = denominator
-
-        int j = 1; 
-
-         
-
-        // to keep the check of no. of terms
-
-        int k = 1; 
-
-             
-
-        // loop till k is not equal to n
-
-        while (k < n)
-
-        {
-
-            j++ ;
-
+            j++;
             k++;
 
-             
-
-            // check if k is already equal to N
-
-            // then the first term is the required
-
-            // rational number
-
-            if (k == n)
-
-                break;
-
-             
-
-            // loop for traversing from right to left
-
-            // downwards diagonally
-
+            // move down-left diagonally
             while (j > 1 && k < n) {
-
                 i++;
-
                 j--;
-
                 k++;
-
             }
 
-             
-
-            if (k == n)
-
-            break;
-
-             
+            if (k >= n) break;
 
             i++;
-
             k++;
 
-             
-
-            if (k == n)
-
-            break;
-
-             
-
-            // loop for traversing from left 
-
-            // to right upwards diagonally
-
+            // move up-right diagonally
             while (i > 1 && k < n) {
-
                 i--;
-
                 j++;
-
                 k++;
-
-            } 
-
-        } 
-
-        System.out.print(i +"/" +j + " ");
-
-    }
-
-     
-
-    // Running code
-
-    public static void main (String[] args) 
-
-    {
-
-        System.out.println("This program outputs the n terms");
-
-	Scanner sc = new Scanner(System.in);	System.out.print("Enter the number of terms you want: ");
-
-        int a = sc.nextInt(); 
-
-        for(int n=1;n<=a;++n){
-
-            georgeCantor(n);
-
+            }
         }
 
-         
-
+        System.out.print(i + "/" + j + " ");
     }
 
-} 
+    public static void main(String[] args) {
 
- 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms you want: ");
+        int a = sc.nextInt();
+
+        for (int n = 1; n <= a; n++) {
+            georgeCantor(n);
+        }
+
+        sc.close();
+    }
+}
